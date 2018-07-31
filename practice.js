47d72445b8cb05eca6f2c182d073011c4f9d15ac
Rina Schwartz <rinadschwartz@protonmail.ch>
2018-07-31 14:08:59 +0000

    automatically backed up by learn

---
+++ practice.js
@@ -0,0 +1,12 @@
+//var word;
+//word = 'bird';
+//console.log(word);
+//word = 'dog';
+//console.log(word);
+//function doNothing(){};
+function sayHello () {
+  console.log('Hello!')
+}
+function sayHelloToIsabel() {
+  console.log("Hello, Isabel!")
+}
